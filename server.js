f4220d88af9a4ddd92548794d0bb2966d723534b
gagan2125 <gagansk2125@gmail.com>
2024-06-26 23:59:18 +0530

    vercel 2 commit

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const formRoutes = require("./routes/FormRoute");
+const multer = require("multer");
+const path = require("path");
 require("dotenv").config();
 
 const PORT = 5000;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 mongoose
   .connect(process.env.MONGO_URL, {
